38f15f38d12d1e249c7c6b83310ecd57f204605e
Noémie Boulanger-Lapointe <78905636+Noemie-BL@users.noreply.github.com>
2021-09-09 15:47:58 +0000

    Added Writing group random list generator

---
+++ WritingGroup_randomList.R
@@ -0,0 +1,23 @@
+#the set.seed is decided depending of the week number
+#For example, week 1 of the writing group is set.seed(1)
+#Until Christmas the due date, and set.seed(), are as followed
+#Sept.17: Week 1
+#Oct. 1: Week 2
+#Oct 15: Week 3
+#Oct 29: Week 4
+#Nov 12: Week 5
+#Nov 26: Week 6
+
+
+install.packages("remotes") #Run only once
+remotes::install_github("smmurphy/convenience") #Run only once
+library(convenience)
+
+set.seed(2) # This is set for the first week, change according to the week
+
+participants <- c("Arna Björt", "Denis", "Vala","Ian", "Ingeborg", 
+            "Inga Svala", "Ingvild", "Isabel", "Laura", "Marteinn",
+            "Mathilde", "Noemie")
+
+randomPairs(list = participants, lname = "You are", rname = "Send to")
+
